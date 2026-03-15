bf2f41b010738bd1de94596f21b356755794bcae
Eirik Rognø <eirik.rogno@gmail.com>
2026-03-15 12:53:33 +0100

    Generate static params

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -2,6 +2,13 @@ import fs from "fs";
 import path from "path";
 import MarkdownRenderer from "@/src/components/MarkdownRenderer";
 
+export async function generateStaticParams() {
+  const slugs = fs.readdirSync('public')
+      .filter((file) => file.endsWith(".md"))
+      .map((file) => ({slug: file.substring(0, file.length - 3)}))
+  return slugs; 
+}
+
 export default async function Page({
   params
 }: { params: Promise<{ slug: string }> }) {
